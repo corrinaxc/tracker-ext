ad3a172807f0d267d04c79d9bf40d6c8e713dce3
Corrina Cornish <corrina.cornish@gmail.com>
2024-08-08 22:58:57 +0100

    event listener for non-full url refreshes

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
 // import useLocalStorage from "use-local-storage";
 
-chrome.webNavigation.onCompleted.addListener(function(details) {
+function logUrl(details) {
     if (details.frameId === 0) { 
         const timestamp = new Date().toISOString(); 
         console.log(`[${timestamp}] URL:`, details.url);
+    }
+}
+
+chrome.webNavigation.onCompleted.addListener(logUrl);
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(logUrl);
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+        const timestamp = new Date().toISOString();
+        console.log(`[${timestamp}] URL:`, changeInfo.url);
     }
 });
 
