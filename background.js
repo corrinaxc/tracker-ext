7b03968101248eabc76399541ff93c28c315d210
Corrina Cornish <corrina.cornish@gmail.com>
2024-08-15 11:50:19 +0300

    removed comments

--- background.js
+++ background.js
@@ -5,10 +5,10 @@ let lastLoggedUrl = '';
 function logUrl(details) {
     if (details.frameId === 0) { 
         const url = details.url;
-        if (url !== lastLoggedUrl) {  // Check if URL is the same as the last logged URL
+        if (url !== lastLoggedUrl) { 
             const timestamp = new Date().toISOString(); 
             console.log(`[${timestamp}] URL:`, url);
-            lastLoggedUrl = url;  // Update the last logged URL
+            lastLoggedUrl = url;
         }
     }
 }
@@ -17,10 +17,10 @@ chrome.webNavigation.onCompleted.addListener(logUrl);
 chrome.webNavigation.onHistoryStateUpdated.addListener(logUrl);
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url && changeInfo.url !== lastLoggedUrl) {  // Same check for tabs.onUpdated
+    if (changeInfo.url && changeInfo.url !== lastLoggedUrl) { 
         const timestamp = new Date().toISOString();
         console.log(`[${timestamp}] URL:`, changeInfo.url);
-        lastLoggedUrl = changeInfo.url;  // Update the last logged URL
+        lastLoggedUrl = changeInfo.url;
     }
 });
 
